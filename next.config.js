ff3e9a01051c2c4b52d7d204dd5b22c00c75c55c
ray panganiban <raybagsik0825@gmail.com>
2026-09-15 15:47:59 +0800

    Configure static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  basePath: process.env.GITHUB_ACTIONS === "true" ? "/barangay-e-service" : "",
 };
 
 module.exports = nextConfig;
